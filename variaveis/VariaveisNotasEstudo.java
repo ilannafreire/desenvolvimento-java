263c13888f0349324f85648bd8ebd1f2e9f39802
Ilanna Freire <ilannafreiret@hotmail.com>
2024-04-23 14:11:13 -0300

    feat: add variaveis e notas

---
+++ variaveis/VariaveisNotasEstudo.java
@@ -0,0 +1,27 @@
+package variaveis;
+
+
+public class VariaveisNotasEstudo {
+    public static void main(String[] args) {
+        // Variáveis que representam a data atual.
+        int dia = 23;
+        int mes = 04;
+        int ano= 2024 ;
+
+        // Atribuir novo valor para o dia.
+        dia = 30;
+
+        System.out.println(dia);
+        System.out.println(mes);
+        System.out.println(ano);
+
+    }
+}
+
+/*
+ * Variáveis são conteineres de dados.
+ * Entre "" você referencia um texto e não uma variável!
+ * Para acessar o valor da variável é só informar o identificador dela.
+ * Se estiver começando a aprender a linguagem, utilize comentários no código.
+ *
+ */
